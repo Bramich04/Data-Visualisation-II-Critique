5d661349445b001ef3fcd4cdd8d5a1bb6ddd8adf
Oliver Bramich <147389067+Bramich04@users.noreply.github.com>
2024-10-09 21:19:57 +1100

    Add files via upload

---
+++ Visualisation_II.js
@@ -0,0 +1,17 @@
+var vg_1 = "Temperature_Visualisation.vega";
+vegaEmbed("#Temperature_Visualisation", vg_1).then(function(result) {}).catch(console.error);
+
+var vg_2 = "Bushfire_Visualisation.vega";
+vegaEmbed("#Bushfire_Visualisation", vg_2).then(function(result) {}).catch(console.error);
+
+var vg_3 = "Environment_Visualisation.vega";
+vegaEmbed("#Environment_Visualisation", vg_3).then(function(result) {}).catch(console.error);
+
+var vg_4 = "BurntArea_Visualisation.vega";
+vegaEmbed("#BurntArea_Visualisation", vg_4).then(function(result) {}).catch(console.error);
+
+var vg_5 = "AnimalClass_Visualisation.vega";
+vegaEmbed("#AnimalClass_Visualisation", vg_5).then(function(result) {}).catch(console.error);
+
+var vg_6 = "AnimalSpecies_Visualisation.vega";
+vegaEmbed("#AnimalSpecies_Visualisation", vg_6).then(function(result) {}).catch(console.error);
